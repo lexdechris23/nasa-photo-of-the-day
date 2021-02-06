2a98af7e2ef77f81aeacf5db4e81b1586312a094
lexdechris23 <70167097+lexdechris23@users.noreply.github.com>
2021-02-06 15:59:33 -0600

    Added styled-components

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styled from 'styled-components';
 
 export default function NasaPhoto (){
 const [title, setTitle] = useState ([]); 
